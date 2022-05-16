b9b5554cc9a969f6a7df0a319c85c1b4616fdb54
marshmallow <marshycity@gmail.com>
2022-05-16 17:23:05 +1000

    remove theme for voting on an option in light mode

--- components/VoteOption.tsx
+++ components/VoteOption.tsx
@@ -7,18 +7,17 @@ const VoteOption: React.FC<{
   option: IOption
   selected: string | undefined
   setSelected: Dispatch<SetStateAction<string | undefined>>
-  colour: string
-}> = ({ option, colour, selected, setSelected }) => {
+}> = ({ option, selected, setSelected }) => {
   return (
     <label
-      className={`w-full text-${colour}-700 dark:text-slate-200 font-medium transition-colors rounded-md flex items-center h-11 cursor-pointer ${
+      className={`w-full text-blue-700 dark:text-slate-200 font-medium transition-colors rounded-md flex items-center h-11 cursor-pointer ${
         selected === option.id ? 'dark:bg-slate-800' : 'hover:dark:bg-slate-800'
       }`}
       onClick={() => setSelected(option.id)}>
       <FontAwesomeIcon
         className={`mx-3 ${
           selected === option.id
-            ? `text-${colour}-600`
+            ? 'text-blue-600'
             : 'text-slate-400 dark:text-slate-600'
         }`}
         size="lg"
