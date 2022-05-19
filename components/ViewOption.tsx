f18c744051208ba3633fdccdf9d4361b87dbaf6e
marshmallow <marshycity@gmail.com>
2022-05-19 21:16:38 +1000

    add exact count of votes

--- components/ViewOption.tsx
+++ components/ViewOption.tsx
@@ -8,7 +8,8 @@ const ViewOption: React.FC<{
   colour: string
 }> = ({ option, max, colour }) => {
   const width = useMemo(() => {
-    const number = option.votes === 0 ? option.votes : (100 * option.votes) / max
+    const number =
+      option.votes === 0 ? option.votes : (100 * option.votes) / max
     return `${number === NaN ? 0 : number.toFixed(0)}%`
   }, [option.votes, max])
 
@@ -17,7 +18,9 @@ const ViewOption: React.FC<{
       <div
         className={`flex justify-between text-${colour}-700 mb-1 items-center gap-2 font-medium dark:text-slate-200`}>
         <span className="break-all text-lg sm:text-2xl">{option.option}</span>
-        <span className="w-fit text-sm sm:text-lg">{width}</span>
+        <span className="w-fit text-sm sm:text-lg">
+          {width} <span className="text-slate-500 dark:text-slate-400">{option.votes > 0 && `(${option.votes})`}</span>
+        </span>
       </div>
       <div className="h-2.5 w-full rounded-full bg-gray-200 dark:bg-gray-700">
         <m.div
