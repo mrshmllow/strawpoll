6775931e08ad3d34cb367e7c37da4305657f8fc6
marshmallow <marshycity@gmail.com>
2022-06-15 20:09:10 +1000

    improved images in the voting view

--- components/VoteOption.tsx
+++ components/VoteOption.tsx
@@ -10,18 +10,16 @@ const VoteOption: React.FC<{
   setSelected: Dispatch<SetStateAction<string | undefined>>
 }> = ({ option, selected, setSelected }) => {
   return (
-    <div>
-      <button
-        className={`flex w-full cursor-pointer items-center break-all rounded-md py-2 pr-2 text-lg font-medium text-blue-700 transition-colors dark:text-slate-200 sm:text-2xl ${
-          selected === option.id
-            ? "dark:bg-slate-800"
-            : "hover:dark:bg-slate-800"
-        }`}
-        onClick={() => setSelected(option.id)}
-        aria-label={option.option}
-        role="radio"
-        aria-checked={selected === option.id}
-        id={option.id}>
+    <button
+      className={`w-full cursor-pointer items-center break-all rounded-md py-2 text-lg font-medium text-blue-700 transition-colors dark:text-slate-200 sm:text-2xl ${
+        selected === option.id ? "dark:bg-slate-800" : "hover:dark:bg-slate-800"
+      } ${option.image ? "px-2" : "pr-2"}`}
+      onClick={() => setSelected(option.id)}
+      aria-label={option.option}
+      role="radio"
+      aria-checked={selected === option.id}
+      id={option.id}>
+      <div className="flex w-full">
         <FontAwesomeIcon
           className={`mx-3 w-5 ${
             selected === option.id
@@ -34,16 +32,16 @@ const VoteOption: React.FC<{
         <label htmlFor={option.id} className="cursor-pointer">
           {option.option}
         </label>
-      </button>
+      </div>
       {option.image && (
         <img
           src={
             supabase.storage.from("polls").getPublicUrl(option.image).publicURL!
           }
-          className="mt-2 rounded-lg"
+          className="mt-2 mx-auto rounded-lg"
         />
       )}
-    </div>
+    </button>
   )
 }
 
